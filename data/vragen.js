b3f3a6aab7b2791dfbbbdb621af54efe1431dccf
Kikker2022 <andries1963@gmail.com>
2026-03-25 16:40:35 +0100

    Add questions and answers to vragen.js

--- data/vragen.js
+++ data/vragen.js
@@ -1,1 +1,16 @@
 
+const alleVragen = [
+  {vraag:"Wat is de hoofdstad van Nederland?", antwoord:"Amsterdam", rubriek:"TopoVMBO", moeilijkheid:1},
+  {vraag:"Welke provincie ligt helemaal in het noorden?", antwoord:"Groningen", rubriek:"TopoVMBO", moeilijkheid:1},
+  {vraag:"Welke zee ligt ten noorden van Nederland?", antwoord:"Noordzee", rubriek:"TopoVMBO", moeilijkheid:2},
+
+  {vraag:"Wat is de hoofdplaats van Ooststellingwerf?", antwoord:"Oosterwolde", rubriek:"Oost15", moeilijkheid:1},
+  {vraag:"In welke provincie ligt Ooststellingwerf?", antwoord:"Friesland", rubriek:"Oost15", moeilijkheid:1},
+
+  {vraag:"Welke plaats ligt bij het Drents-Friese Wold?", antwoord:"Appelscha", rubriek:"Oost40", moeilijkheid:2},
+
+  {vraag:"Wat meet een tachymeter?", antwoord:"Afstanden en hoeken", rubriek:"Landmeetkunde", moeilijkheid:3},
+  {vraag:"Waar staat RTK voor?", antwoord:"Realtime kinematic", rubriek:"Landmeetkunde", moeilijkheid:3}
+];
+
+export default alleVragen;
