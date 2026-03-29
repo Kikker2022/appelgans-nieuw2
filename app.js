262c5e0a82433d55956097798e4b25e81ae9f30e
Kikker2022 <andries1963@gmail.com>
2026-03-29 11:35:48 +0200

    Fix variable name for question selection

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ function gooiDobbelsteen() {
   let worp = Math.floor(Math.random() * 6) + 1;
   document.getElementById("dobbel").innerText = "Je gooide: " + worp;
 
-  let vraag = alleVragen[Math.floor(Math.random() * alleVragen.length)];
+  let vraag = vragen[Math.floor(Math.random() * vragen.length)];
+
   document.getElementById("vraag").innerText = vraag.vraag;
+  document.getElementById("antwoord").innerText = vraag.antwoord;
 
 }
 
-window.gooiDobbelsteen = gooiDobbelsteen; 
+window.gooiDobbelsteen = gooiDobbelsteen;
