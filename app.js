57bef51f8bda655956337400bdb898684a2e3d0a
Kikker2022 <andries1963@gmail.com>
2026-03-29 11:25:08 +0200

    Update random question selection in app.js

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ function gooiDobbelsteen() {
   let worp = Math.floor(Math.random() * 6) + 1;
   document.getElementById("dobbel").innerText = "Je gooide: " + worp;
 
-  let vraag = vragen[Math.floor(Math.random() * vragen.length)];
+  let vraag = alleVragen[Math.floor(Math.random() * alleVragen.length)];
   document.getElementById("vraag").innerText = vraag.vraag;
 
 }
 
 window.gooiDobbelsteen = gooiDobbelsteen; 
-
