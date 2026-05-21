c6ca728b0f1c6ffde79cc6e2aa488b116b770592
Kikker2022 <andries1963@gmail.com>
2026-05-21 21:45:08 +0200

    Update vragen.js

--- data/vragen.js
+++ data/vragen.js
@@ -1,5 +1,7 @@
 const vragen = [
 
+/// OOSTSTELLINGWERF
+
 {
 categorie:"Ooststellingwerf",
 
@@ -11,7 +13,7 @@ c:"Oosterwolde",
 
 correct:"b",
 
-uitleg:"Appelscha is het bekendste toeristische dorp van Ooststellingwerf, onder andere door het Drents-Friese Wold, de Bosbergtoren en de recreatieparken."
+uitleg:"Appelscha is het bekendste toeristische dorp van Ooststellingwerf."
 },
 
 {
@@ -25,7 +27,7 @@ c:"Vanwege wolkenkrabbers",
 
 correct:"b",
 
-uitleg:"Appelscha staat bekend om natuurtoerisme, campings, fietsroutes en rustige recreatiegebieden."
+uitleg:"Appelscha staat bekend om natuurtoerisme, campings en fietsroutes."
 },
 
 {
@@ -39,7 +41,7 @@ c:"Het Drents-Friese Wold",
 
 correct:"c",
 
-uitleg:"Het Drents-Friese Wold behoort tot de grootste natuurgebieden van Nederland en ligt op de grens van Friesland en Drenthe."
+uitleg:"Het Drents-Friese Wold behoort tot de grootste natuurgebieden van Nederland."
 },
 
 {
@@ -53,7 +55,7 @@ c:"De Kale Duinentoren",
 
 correct:"b",
 
-uitleg:"De Bosbergtoren is één van de bekendste herkenningspunten van Appelscha en biedt uitzicht over Friesland en Drenthe."
+uitleg:"De Bosbergtoren biedt uitzicht over Friesland en Drenthe."
 },
 
 {
@@ -67,7 +69,7 @@ c:"Roeien",
 
 correct:"a",
 
-uitleg:"De motorcross in Appelscha is al jarenlang populair en trekt deelnemers en publiek uit heel Nederland."
+uitleg:"De motorcross in Appelscha trekt bezoekers uit heel Nederland."
 },
 
 {
@@ -81,13 +83,13 @@ c:"Diepzeeduiken",
 
 correct:"b",
 
-uitleg:"Het Drents-Friese Wold heeft uitgebreide mountainbikeroutes die populair zijn bij sportieve bezoekers."
+uitleg:"Het Drents-Friese Wold heeft populaire mountainbikeroutes."
 },
 
 {
 categorie:"Ooststellingwerf",
 
-vraag:"Welke taal of streektaal hoor je veel in Ooststellingwerf?",
+vraag:"Welke streektaal hoor je veel in Ooststellingwerf?",
 
 a:"Stellingwerfs",
 b:"Limburgs",
@@ -95,7 +97,7 @@ c:"Twents",
 
 correct:"a",
 
-uitleg:"Het Stellingwerfs is een Nedersaksische streektaal die veel gesproken wordt in Oost- en Weststellingwerf."
+uitleg:"Het Stellingwerfs behoort tot het Nedersaksisch."
 },
 
 {
@@ -109,13 +111,13 @@ c:"Zeilen",
 
 correct:"b",
 
-uitleg:"Door de vele natuurgebieden is Appelscha erg populair bij wandelaars en fietsers."
+uitleg:"Appelscha is populair bij wandelaars en fietsers."
 },
 
 {
 categorie:"Ooststellingwerf",
 
-vraag:"Hoe heet het bezoekerscentrum bij het Drents-Friese Wold in Appelscha?",
+vraag:"Hoe heet het bezoekerscentrum bij het Drents-Friese Wold?",
 
 a:"Buitencentrum Drents-Friese Wold",
 b:"Natuurhuis Friesland",
@@ -123,21 +125,7 @@ c:"Eco Centrum Appelscha",
 
 correct:"a",
 
-uitleg:"Het Buitencentrum Drents-Friese Wold organiseert excursies en geeft informatie over natuur en recreatie."
-},
-
-{
-categorie:"Ooststellingwerf",
-
-vraag:"Welke bekende sportevenementen worden regelmatig in Appelscha georganiseerd?",
-
-a:"Skiwedstrijden",
-b:"ATB- en wielerevenementen",
-c:"Zeilkampioenschappen",
-
-correct:"b",
-
-uitleg:"Door de bosrijke omgeving worden in Appelscha regelmatig mountainbike- en wielerevenementen gehouden."
+uitleg:"Het buitencentrum organiseert excursies en natuuractiviteiten."
 },
 
 {
@@ -151,175 +139,575 @@ c:"Hennie Kuiper",
 
 correct:"b",
 
-uitleg:"Johan van der Velde woonde jarenlang in Appelscha en was een succesvol profwielrenner."
+uitleg:"Johan van der Velde woonde jarenlang in Appelscha."
 },
 
-{
-categorie:"Ooststellingwerf",
-
-vraag:"Welke zandvlakte bij Appelscha staat bekend als ‘Friese Sahara’?",
-
-a:"De Kale Duinen",
-b:"Het Fochteloërveen",
-c:"De Holtingerveldse Heide",
-
-correct:"a",
-
-uitleg:"De Kale Duinen bestaan uit stuifzandgebieden en lijken op een kleine woestijn."
-},
-
-{
-categorie:"Drenthe",
-
-vraag:"Welke provincie grenst direct aan Appelscha?",
-
-a:"Groningen",
-b:"Overijssel",
-c:"Drenthe",
-
-correct:"c",
-
-uitleg:"Appelscha ligt vlak tegen de provincie Drenthe aan."
-},
-
-{
-categorie:"Drenthe",
-
-vraag:"Welke plaats ligt het dichtst bij Assen?",
-
-a:"Boven Smilde",
-b:"Hijker Smilde",
-c:"Hooger Smilde",
-
-correct:"a",
-
-uitleg:"Boven Smilde ligt het dichtst bij Assen."
-},
+/// FRYSLAN
 
 {
 categorie:"Fryslân",
 
-vraag:"Welke Friese schrijver en dichter kwam uit de Stellingwerven-regio?",
+vraag:"Wat is de hoofdstad van Fryslân?",
 
-a:"Fedde Schurer",
-b:"Piter Boersma",
-c:"Johannes Vollenhove",
-
-correct:"a",
-
-uitleg:"Fedde Schurer was een bekende Friese schrijver en dichter."
-},
-
-{
-categorie:"Fryslân",
-
-vraag:"Welke plaats is de hoofdplaats van Ooststellingwerf?",
-
-a:"Oosterwolde",
-b:"Appelscha",
-c:"Oldeberkoop",
-
-correct:"a",
-
-uitleg:"Oosterwolde is het bestuurlijk centrum van Ooststellingwerf."
-},
-
-{
-categorie:"Fryslân",
-
-vraag:"Waar is Grutte Pier geboren?",
-
-a:"Haulerwijk",
-b:"Appelscha",
-c:"Kimswerd",
-
-correct:"c",
-
-uitleg:"Grutte Pier werd geboren in Kimswerd."
-},
-
-{
-categorie:"Groningen",
-
-vraag:"Welk natuurgebied ligt bij Appelscha en is bekend om kraanvogels?",
-
-a:"Fochteloërveen",
-b:"Alde Feanen",
-c:"Lauwersmeer",
-
-correct:"a",
-
-uitleg:"Het Fochteloërveen is een bijzonder hoogveengebied."
-},
-
-{
-categorie:"Groningen",
-
-vraag:"Welk dorp werd in 2011 ‘Leukste dorp van Groningen’?",
-
-a:"Zevenhuizen",
-b:"Winsum",
-c:"Niekerk",
-
-correct:"a",
-
-uitleg:"Zevenhuizen kreeg deze titel in 2011."
-},
-
-{
-categorie:"Educatief",
-
-vraag:"Welke kleur heeft de Bosbergtoren grotendeels?",
-
-a:"Rood",
-b:"Zwart",
-c:"Wit",
-
-correct:"a",
-
-uitleg:"De Bosbergtoren is herkenbaar door zijn rode kleur."
-},
-
-{
-categorie:"Educatief",
-
-vraag:"Wat kenmerkt de Kale Duinen?",
-
-a:"Hoge bergen",
-b:"Stuifzand en heide",
-c:"Grote meren",
+a:"Sneek",
+b:"Leeuwarden",
+c:"Drachten",
 
 correct:"b",
 
-uitleg:"De Kale Duinen bestaan uit zand en heide."
+uitleg:"Leeuwarden is de hoofdstad van Fryslân."
+},
+
+{
+categorie:"Fryslân",
+
+vraag:"Welke taal is officieel erkend in Fryslân?",
+
+a:"Drents",
+b:"Fries",
+c:"Gronings",
+
+correct:"b",
+
+uitleg:"Het Fries is een officiële rijkstaal."
+},
+
+{
+categorie:"Fryslân",
+
+vraag:"Welke beroemde tocht verbindt elf Friese steden?",
+
+a:"Friese Merenroute",
+b:"Elfstedentocht",
+c:"Noorderronde",
+
+correct:"b",
+
+uitleg:"De Elfstedentocht is wereldwijd bekend."
+},
+
+{
+categorie:"Fryslân",
+
+vraag:"Welke Friese sport wordt met traditionele zeilboten gehouden?",
+
+a:"Skûtsjesilen",
+b:"Curling",
+c:"Suppen",
+
+correct:"a",
+
+uitleg:"Skûtsjesilen is een traditionele Friese watersport."
+},
+
+{
+categorie:"Fryslân",
+
+vraag:"Welke Friese held vocht tegen de Hollanders?",
+
+a:"Willem Barentsz",
+b:"Grutte Pier",
+c:"Michiel de Ruyter",
+
+correct:"b",
+
+uitleg:"Grutte Pier is één van de bekendste Friese volkshelden."
+},
+
+{
+categorie:"Fryslân",
+
+vraag:"Welke stad was Culturele Hoofdstad van Europa in 2018?",
+
+a:"Drachten",
+b:"Franeker",
+c:"Leeuwarden",
+
+correct:"c",
+
+uitleg:"Leeuwarden-Friesland was Culturele Hoofdstad van Europa."
+},
+
+{
+categorie:"Fryslân",
+
+vraag:"Welke watersportplaats ligt in Fryslân?",
+
+a:"Sneek",
+b:"Emmen",
+c:"Apeldoorn",
+
+correct:"a",
+
+uitleg:"Sneek staat bekend om watersport."
+},
+
+{
+categorie:"Fryslân",
+
+vraag:"Welke Waddeneilanden horen bij Fryslân?",
+
+a:"Texel en Vlieland",
+b:"Ameland en Schiermonnikoog",
+c:"Texel en Terschelling",
+
+correct:"b",
+
+uitleg:"Ameland en Schiermonnikoog horen bij Fryslân."
+},
+
+{
+categorie:"Fryslân",
+
+vraag:"Wat is een skûtsje?",
+
+a:"Een schaats",
+b:"Een Friese zeilboot",
+c:"Een molen",
+
+correct:"b",
+
+uitleg:"Een skûtsje is een traditioneel Fries vrachtschip."
+},
+
+{
+categorie:"Fryslân",
+
+vraag:"Welke Friese schrijver was ook journalist?",
+
+a:"Fedde Schurer",
+b:"Theo Thijssen",
+c:"Multatuli",
+
+correct:"a",
+
+uitleg:"Fedde Schurer was schrijver, dichter en journalist."
+},
+
+/// GRONINGEN
+
+{
+categorie:"Groningen",
+
+vraag:"Wat is de hoofdstad van Groningen?",
+
+a:"Veendam",
+b:"Winschoten",
+c:"Groningen",
+
+correct:"c",
+
+uitleg:"De stad Groningen is de hoofdstad van de provincie."
+},
+
+{
+categorie:"Groningen",
+
+vraag:"Welke universiteit staat in Groningen?",
+
+a:"TU Groningen",
+b:"Rijksuniversiteit Groningen",
+c:"Noordelijke Universiteit",
+
+correct:"b",
+
+uitleg:"De Rijksuniversiteit Groningen bestaat sinds 1614."
+},
+
+{
+categorie:"Groningen",
+
+vraag:"Welke toren staat bekend als de Olle Grieze?",
+
+a:"Martinitoren",
+b:"Achmeatoren",
+c:"Domtoren",
+
+correct:"a",
+
+uitleg:"De Martinitoren wordt lokaal Olle Grieze genoemd."
+},
+
+{
+categorie:"Groningen",
+
+vraag:"Waar staat Groningen bekend om?",
+
+a:"Gaswinning",
+b:"Diamantwinning",
+c:"Goudmijnen",
+
+correct:"a",
+
+uitleg:"Groningen is bekend vanwege aardgaswinning."
+},
+
+{
+categorie:"Groningen",
+
+vraag:"Welke zee grenst aan Groningen?",
+
+a:"Noordzee",
+b:"Waddenzee",
+c:"Zwarte Zee",
+
+correct:"b",
+
+uitleg:"De Waddenzee ligt aan de noordkant van Groningen."
+},
+
+{
+categorie:"Groningen",
+
+vraag:"Welke voetbalclub speelt betaald voetbal in Groningen?",
+
+a:"FC Emmen",
+b:"SC Cambuur",
+c:"FC Groningen",
+
+correct:"c",
+
+uitleg:"FC Groningen speelt in het betaalde voetbal."
+},
+
+{
+categorie:"Groningen",
+
+vraag:"Welke provincie grenst aan Groningen?",
+
+a:"Drenthe",
+b:"Limburg",
+c:"Zeeland",
+
+correct:"a",
+
+uitleg:"Drenthe ligt direct onder Groningen."
+},
+
+{
+categorie:"Groningen",
+
+vraag:"Welke plaats won de titel Mooiste dorp van Nederland?",
+
+a:"Winsum",
+b:"Leek",
+c:"Bedum",
+
+correct:"a",
+
+uitleg:"Winsum won deze titel van de ANWB."
+},
+
+{
+categorie:"Groningen",
+
+vraag:"Welke vogel broedt in het Fochteloërveen?",
+
+a:"Papegaai",
+b:"Kraanvogel",
+c:"Flamingo",
+
+correct:"b",
+
+uitleg:"Kraanvogels broeden weer in het Fochteloërveen."
+},
+
+{
+categorie:"Groningen",
+
+vraag:"Welke stad staat bekend als studentenstad?",
+
+a:"Assen",
+b:"Groningen",
+c:"Drachten",
+
+correct:"b",
+
+uitleg:"Groningen heeft veel studenten."
+},
+
+/// DRENTHE
+
+{
+categorie:"Drenthe",
+
+vraag:"Wat is de hoofdstad van Drenthe?",
+
+a:"Emmen",
+b:"Assen",
+c:"Hoogeveen",
+
+correct:"b",
+
+uitleg:"Assen is de hoofdstad van Drenthe."
+},
+
+{
+categorie:"Drenthe",
+
+vraag:"Waar staat Drenthe vooral bekend om?",
+
+a:"Hunebedden",
+b:"Wolkenkrabbers",
+c:"Deltawerken",
+
+correct:"a",
+
+uitleg:"Drenthe heeft de meeste hunebedden van Nederland."
+},
+
+{
+categorie:"Drenthe",
+
+vraag:"Welke dierentuin ligt in Drenthe?",
+
+a:"Wildlands",
+b:"Artis",
+c:"Blijdorp",
+
+correct:"a",
+
+uitleg:"Wildlands Adventure Zoo ligt in Emmen."
+},
+
+{
+categorie:"Drenthe",
+
+vraag:"Welke motorsportwedstrijd wordt in Assen gehouden?",
+
+a:"TT Assen",
+b:"Grand Prix Zwolle",
+c:"Moto Emmen",
+
+correct:"a",
+
+uitleg:"TT Assen is wereldwijd bekend."
+},
+
+{
+categorie:"Drenthe",
+
+vraag:"Welke plaats ligt dicht bij Appelscha?",
+
+a:"Diever",
+b:"Harlingen",
+c:"Leeuwarden",
+
+correct:"a",
+
+uitleg:"Diever ligt vlak over de grens in Drenthe."
+},
+
+{
+categorie:"Drenthe",
+
+vraag:"Welke natuurgebied ligt deels in Drenthe?",
+
+a:"Biesbosch",
+b:"Drents-Friese Wold",
+c:"Oosterschelde",
+
+correct:"b",
+
+uitleg:"Het Drents-Friese Wold ligt deels in Drenthe."
+},
+
+{
+categorie:"Drenthe",
+
+vraag:"Welke provincie ligt onder Drenthe?",
+
+a:"Overijssel",
+b:"Zeeland",
+c:"Noord-Holland",
+
+correct:"a",
+
+uitleg:"Overijssel grenst aan de zuidkant van Drenthe."
+},
+
+{
+categorie:"Drenthe",
+
+vraag:"Waarvoor werd turf vroeger gebruikt?",
+
+a:"Brandstof",
+b:"Speelgoed",
+c:"Papier",
+
+correct:"a",
+
+uitleg:"Turf werd gebruikt om huizen te verwarmen."
+},
+
+{
+categorie:"Drenthe",
+
+vraag:"Welke sport trekt veel bezoekers naar Assen?",
+
+a:"Zeilen",
+b:"Motorrennen",
+c:"Skiën",
+
+correct:"b",
+
+uitleg:"De TT trekt jaarlijks veel motorsportfans."
+},
+
+{
+categorie:"Drenthe",
+
+vraag:"Welke wandelroute loopt door Drenthe?",
+
+a:"Pieterpad",
+b:"Deltapad",
+c:"Kustroute",
+
+correct:"a",
+
+uitleg:"Het Pieterpad loopt door meerdere Drentse gebieden."
+},
+
+/// EDUCATIEF
+
+{
+categorie:"Educatief",
+
+vraag:"Hoeveel provincies heeft Nederland?",
+
+a:"10",
+b:"12",
+c:"14",
+
+correct:"b",
+
+uitleg:"Nederland bestaat uit twaalf provincies."
 },
 
 {
 categorie:"Educatief",
 
-vraag:"Welke gemeente grenst aan Ooststellingwerf?",
+vraag:"Welke planeet heet de rode planeet?",
 
-a:"Westerveld",
-b:"Dantumadiel",
-c:"Waadhoeke",
+a:"Mars",
+b:"Venus",
+c:"Jupiter",
 
 correct:"a",
 
-uitleg:"Westerveld ligt direct naast Ooststellingwerf."
+uitleg:"Mars lijkt rood door ijzeroxide."
 },
 
 {
 categorie:"Educatief",
 
-vraag:"Wat maakt het Fochteloërveen bijzonder?",
+vraag:"Hoeveel dagen telt een schrikkeljaar?",
 
-a:"Laatste hoogveen van West-Europa",
-b:"Actieve vulkanen",
-c:"Onder zeeniveau",
+a:"365",
+b:"366",
+c:"364",
+
+correct:"b",
+
+uitleg:"Een schrikkeljaar heeft een extra dag in februari."
+},
+
+{
+categorie:"Educatief",
+
+vraag:"Wat is de hoofdstad van Duitsland?",
+
+a:"Hamburg",
+b:"München",
+c:"Berlijn",
+
+correct:"c",
+
+uitleg:"Berlijn is de hoofdstad van Duitsland."
+},
+
+{
+categorie:"Educatief",
+
+vraag:"Welke taal spreekt men vooral in Brazilië?",
+
+a:"Spaans",
+b:"Portugees",
+c:"Italiaans",
+
+correct:"b",
+
+uitleg:"Brazilië was vroeger een Portugese kolonie."
+},
+
+{
+categorie:"Educatief",
+
+vraag:"Hoeveel minuten zitten er in een uur?",
+
+a:"60",
+b:"90",
+c:"100",
 
 correct:"a",
 
-uitleg:"Het is één van de laatste hoogveengebieden van West-Europa."
+uitleg:"Een uur bestaat uit zestig minuten."
+},
+
+{
+categorie:"Educatief",
+
+vraag:"Wat is het grootste oceaan ter wereld?",
+
+a:"Atlantische Oceaan",
+b:"Indische Oceaan",
+c:"Stille Oceaan",
+
+correct:"c",
+
+uitleg:"De Stille Oceaan is het grootste oceaangebied."
+},
+
+{
+categorie:"Educatief",
+
+vraag:"Welke kleur krijg je door blauw en geel te mengen?",
+
+a:"Paars",
+b:"Groen",
+c:"Oranje",
+
+correct:"b",
+
+uitleg:"Blauw en geel vormen samen groen."
+},
+
+{
+categorie:"Educatief",
+
+vraag:"Wat is de langste rivier van Europa?",
+
+a:"Donau",
+b:"Rijn",
+c:"Wolga",
+
+correct:"c",
+
+uitleg:"De Wolga is de langste rivier van Europa."
+},
+
+{
+categorie:"Educatief",
+
+vraag:"Welke gas hebben mensen nodig om te leven?",
+
+a:"Waterstof",
+b:"Zuurstof",
+c:"Helium",
+
+correct:"b",
+
+uitleg:"Mensen hebben zuurstof nodig om te ademen."
 }
 
 ];
