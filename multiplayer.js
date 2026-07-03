a22098b0fa31df80f56a344395f2632f2e16d499
Kikker2022 <andries1963@gmail.com>
2026-07-03 14:01:35 +0200

    Add joinGame function for multiplayer game

--- multiplayer.js
+++ multiplayer.js
@@ -24,3 +24,38 @@ function createGame() {
     alert("Spel aangemaakt! Code: " + code);
 
 }
+
+function joinGame() {
+
+    const code =
+        document.getElementById("joinCode").value;
+
+    const name =
+        document.getElementById("joinName").value;
+
+    if (!code || !name) {
+        alert("Vul naam en code in");
+        return;
+    }
+
+    const playersRef =
+        firebase.database().ref("games/" + code + "/players");
+
+    playersRef.once("value").then(snapshot => {
+
+        const count = snapshot.numChildren();
+
+        if (count >= 4) {
+            alert("Spel is vol");
+            return;
+        }
+
+        const playerId = "p" + Date.now();
+
+        playersRef.child(playerId).set({
+            name: name
+        });
+
+        alert("Je zit in het spel!");
+    });
+}
